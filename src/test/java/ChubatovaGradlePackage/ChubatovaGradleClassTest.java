44982d1eaa8d88207e968d929b77a4d4cf0f13b3
AChubatova <n.chubatova@gmail.com>
2020-12-10 16:13:10 +0100

    Initial commit

---
+++ src/test/java/ChubatovaGradlePackage/ChubatovaGradleClassTest.java
@@ -0,0 +1,40 @@
+package ChubatovaGradlePackage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ChubatovaGradleClassTest {
+
+
+
+    @Test
+    public void FizzBuzzNormalNumbers() {
+
+        ChubatovaGradleClass fb = new ChubatovaGradleClass();
+        Assert.assertEquals("1", fb.convert(2));
+        Assert.assertEquals("2", fb.convert(2));
+    }
+
+    @Test
+    public void FizzBuzzThreeNumbers() {
+
+        ChubatovaGradleClass fb = new ChubatovaGradleClass();
+        Assert.assertEquals("Fizz", fb.convert(3));
+    }
+
+    @Test
+    public void FizzBuzzFiveNumbers() {
+
+        ChubatovaGradleClass fb = new ChubatovaGradleClass();
+        Assert.assertEquals("Buzz", fb.convert(5));
+    }
+
+    @Test
+    public void FizzBuzzThreeAndFiveNumbers() {
+
+        ChubatovaGradleClass fb = new ChubatovaGradleClass();
+        Assert.assertEquals("Buzz", fb.convert(5));
+    }
+
+
+}
