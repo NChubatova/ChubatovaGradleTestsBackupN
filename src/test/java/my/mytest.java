86ceb60b3b9ef4eb863c329e0d6fc5fe92047cf6
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-12-10 18:24:10 +0100

    Update mytest.java

--- src/test/java/my/mytest.java
+++ src/test/java/my/mytest.java
@@ -8,7 +8,7 @@ public class mytest {
 
     @Test
     public void foo() {
-        if (System.currentTimeMillis()%3==0){
+        if (System.currentTimeMillis()%4==0){
 
             System.out.println(System.currentTimeMillis());
             System.out.println(System.currentTimeMillis()%2);
