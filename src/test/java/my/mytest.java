b283319491f570ef9ce29b717fc72d09d4b9f54e
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-12-21 14:24:07 +0100

    Update mytest.java

--- src/test/java/my/mytest.java
+++ src/test/java/my/mytest.java
@@ -9,7 +9,15 @@ public class mytest {
 
     @Test
     public void foo1() {
-        if (System.currentTimeMillis()%3==100){
+        if (System.currentTimeMillis()%1==1){
+            try 
+            {
+                Thread.sleep(40000);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
 
         }
         else {
