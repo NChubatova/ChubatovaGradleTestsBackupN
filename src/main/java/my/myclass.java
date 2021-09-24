67da263f519820b34a7e406adf889a5062ab50fe
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-09-24 17:53:13 +0200

    Update myclass.java

--- src/main/java/my/myclass.java
+++ src/main/java/my/myclass.java
@@ -4,6 +4,6 @@ package my;
 public class myclass {
     public static void main(String[] args) {
         int data=50;
-        System.out.println("Hi from myclass4");
+        System.out.println("Hi from myclass45");
     }
 }
