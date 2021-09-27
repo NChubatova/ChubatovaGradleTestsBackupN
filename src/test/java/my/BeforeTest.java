7eacdebe7e8bd7dab5a9ee6592d4313fcd7a2165
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-09-27 16:22:36 +0200

    Create BeforeTest.java

---
+++ src/test/java/my/BeforeTest.java
@@ -0,0 +1,20 @@
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.Thread;
+
+public class BeforeTest {
+
+    @Before
+    public void init() {
+         Assert.fail("fail message");
+
+    }
+
+    @After
+    public void teardown() {
+         Assert.fail("fail message");
+
+    }
+}
