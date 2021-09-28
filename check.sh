315adb47f5d8a84af1d3107760175ceab7ff8227
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-09-28 16:34:10 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 function runInInternalBuilder() {
 docker run custom-gradle $@
-#./gradlew test --stacktrace --no-daemon
 
 }
 function gradleInBuilder() {
     runInInternalBuilder ./gradlew ${GRADLE_RUN_PARAMS} $@
 }
 set -e
-gradleInBuilder test "$@"
+gradleInBuilder test --stacktrace --scan "$@"
