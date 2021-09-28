423104da936020e4c94808f3f38dbf7ec897af95
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-09-28 15:59:37 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 function runInInternalBuilder() {
-docker run custom-gradle /q/gradlew test --stacktrace --no-daemon
+docker run custom-gradle /q/a.sh
+#/q/gradlew test --stacktrace --no-daemon
 }
 function gradleInBuilder() {
     runInInternalBuilder
