9f4b53243ceec40e1e7841f9ef05a76ea5c844ed
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-09-30 14:24:56 +0200

    Update mytest.java

--- src/test/java/my/mytest.java
+++ src/test/java/my/mytest.java
@@ -12,7 +12,7 @@ public class mytest {
         if (System.currentTimeMillis()%3==1){
         }
         else {
-            Assert.fail("fail messssadges4d");
+            Assert.fail("fail messssjadges4d");
 
         };
 
