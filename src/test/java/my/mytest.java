a322bdc41a684c0522a23fc3a2ff86a69af4e5af
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-01 21:54:11 +0100

    Update mytest.java

--- src/test/java/my/mytest.java
+++ src/test/java/my/mytest.java
@@ -1,47 +1,13 @@
 package my;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 import java.lang.*;
 
 public class mytest {
 
     @Test
-    public void foo12() {
-        if (System.currentTimeMillis()%2==1){
-        }
-        else {
-            Assert.fail("fail mkk");
-
-        };
-
-    };
-    @Test
-    public void foo22() {
-        try 
-            {
-                Thread.sleep(3);
-            } 
-            catch(InterruptedException e)
-            {
-            // this part is executed when an exception (in this example InterruptedException) occurs
-            }
-        if (System.currentTimeMillis()%3==1){
-        }
-        else {
-          //
-        };
-
-    };
-    @Test
-    public void foo32() {
-        if (System.currentTimeMillis()%3==1){
-        }
-        else {
-            Assert.fail("fail messajge2");
-        };
-
-    };
+public void testAdd() {
+    assertEquals(42, Integer.sum(19, 23));
+};
 
 }
