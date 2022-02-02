255e8125ddfda56e39e5368e0efebb7fa2ac8321
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-02 12:17:14 +0100

    Update mytest.java

--- src/test/java/my/mytest.java
+++ src/test/java/my/mytest.java
@@ -1,16 +1,32 @@
 package my;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.*;
+import org.junit.runners.*;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 import java.lang.*;
 
-public class mytest {
 
-@ParameterizedTest
-@ValueSource(ints = {1, 3, 5, -3, 15, Integer.MAX_VALUE}) // six numbers
-void isOdd_ShouldReturnTrueForOddNumbers(int number) {
-    assertTrue(Numbers.isOdd(number));
-}
-}
+@RunWith(Parameterized.class)
+ public class mytest {
+     @Parameters(name= "{index}: fib[{0}]={1}")
+     public static Iterable<Object[]> data() {
+         return Arrays.asList(new Object[][] { { 0, 0 }, { 1, 1 }, { 2, 1 },
+                 { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 } });
+     }
+
+     private int fInput;
+
+     private int fExpected;
+
+     public FibonacciTest(int input, int expected) {
+         fInput= input;
+         fExpected= expected;
+     }
+
+     @Test
+     public void test() {
+         assertEquals(fExpected, Fibonacci.compute(fInput));
+     }
+ }
