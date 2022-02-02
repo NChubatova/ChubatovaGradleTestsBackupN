35ece5b0c64e32ecd4f668d1e34c3c5c487c6701
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-02 11:44:21 +0100

    Update mytest.java

--- src/test/java/my/mytest.java
+++ src/test/java/my/mytest.java
@@ -1,6 +1,6 @@
 package my;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.lang.*;
