b20a74260cc782c3872b9262140a380caa66641b
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-03 21:29:23 +0100

    Create mytest1.java

---
+++ src/test/java/my/mytest1.java
@@ -0,0 +1,22 @@
+package my;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
+
+public class mytest1 {
+
+    @Test
+    public void foo2master() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+            Assert.fail("fail mkk");
+
+        //};
+
+    };
+
+
+}
