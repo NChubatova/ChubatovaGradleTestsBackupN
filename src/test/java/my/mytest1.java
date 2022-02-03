02d733f97ffee7caecbad39963836a67c6ba6f49
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-03 15:16:26 +0100

    Create mytest1.java

---
+++ src/test/java/my/mytest1.java
@@ -0,0 +1,47 @@
+package my;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
+
+public class mytest {
+
+    @Test
+    public void foo12() {
+        if (System.currentTimeMillis()%2==1){
+        }
+        else {
+            Assert.fail("fail mk4");
+
+        };
+
+    };
+    @Test
+    public void foo22() {
+        try 
+            {
+                Thread.sleep(3);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+          //
+        };
+
+    };
+    @Test
+    public void foo32() {
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+            Assert.fail("fail messajge2");
+        };
+
+    };
+
+}
