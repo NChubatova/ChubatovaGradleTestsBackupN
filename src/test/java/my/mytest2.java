644b3535998cea8eeed67a085784d93ac7074b92
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-04 10:22:15 +0100

    Add files via upload

---
+++ src/test/java/my/mytest2.java
@@ -0,0 +1,48 @@
+package my;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
+
+public class mytest2 {
+
+    @Test
+    public void test21c() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+            Assert.fail("fail mk");
+System.out.print("GfG1");
+        //};
+
+    };
+    @Test
+    public void test22c() {
+        try 
+            {
+                Thread.sleep(60);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+          //
+        };
+
+    };
+    @Test
+    public void test23c() {
+        if (System.currentTimeMillis()%1==1){
+        }
+        else {
+            Assert.fail("fail messajge2");
+            System.out.print("GfG1");
+        };
+
+    };
+
+}
