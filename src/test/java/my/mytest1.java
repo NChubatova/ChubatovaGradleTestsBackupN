7fc16a9b137d5023095baf9ea74f1144b4705bfa
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-09 11:03:24 +0100

    Update mytest1.java

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -22,7 +22,7 @@ System.out.print("GfG1d");
         try 
             {
                 //Thread.sleep(180000);
-            Thread.sleep(18);
+            Thread.sleep(90000);
             } 
             catch(InterruptedException e)
             {
