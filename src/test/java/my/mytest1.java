468873879de6bacdb29ab202acf4ea2b4b7bf517
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-01 13:13:17 +0100

    Update mytest1.java

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -7,13 +7,16 @@ import java.lang.*;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.junit.Rule;
+org.junit.contrib.java.lang.system.SystemOutRule
 
 
 
 public class mytest1 {
 Logger log = LogManager.getLogger();
-    @Rule
+    
+@Rule
 public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    
     @Test
     public void test11c() {
         //if (System.currentTimeMillis()%3==1){
