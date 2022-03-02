8a94fd25ea39e357b5227f185d139dcea4b874c6
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-02 14:35:29 +0100

    Update mytest1.java

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -27,7 +27,7 @@ public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
          try 
             {
                 //Thread.sleep(90000);
-            Thread.sleep(30000);
+            Thread.sleep(15000);
             } 
             catch(InterruptedException e)
             {
@@ -43,7 +43,7 @@ public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
         try 
             {
                 //Thread.sleep(180000);
-            Thread.sleep(30000);
+            Thread.sleep(15000);
             } 
             catch(InterruptedException e)
             {
@@ -61,7 +61,7 @@ public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
                  try 
             {
                 //Thread.sleep(90000);
-            Thread.sleep(30000);
+            Thread.sleep(15000);
             } 
             catch(InterruptedException e)
             {
