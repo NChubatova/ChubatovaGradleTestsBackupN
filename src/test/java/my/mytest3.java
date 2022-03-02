3d801b9fdc4ada40af11faabc217d91d67291e4c
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-02 14:50:00 +0100

    Update mytest3.java

--- src/test/java/my/mytest3.java
+++ src/test/java/my/mytest3.java
@@ -12,7 +12,7 @@ public class mytest3 {
                  try 
             {
                 //Thread.sleep(90000);
-            Thread.sleep(15000);
+            Thread.sleep(1500);
             } 
             catch(InterruptedException e)
             {
@@ -31,7 +31,7 @@ System.out.print("GfG1s");
         try 
             {
                 //Thread.sleep(90000);
-            Thread.sleep(15000);
+            Thread.sleep(1500);
             } 
             catch(InterruptedException e)
             {
@@ -49,7 +49,7 @@ System.out.print("GfG1s");
                  try 
             {
                 //Thread.sleep(90000);
-            Thread.sleep(15000);
+            Thread.sleep(1500);
             } 
             catch(InterruptedException e)
             {
