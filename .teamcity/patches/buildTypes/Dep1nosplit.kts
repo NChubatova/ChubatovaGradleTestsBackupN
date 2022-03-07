04d9b367c8f41ef552755c4c0f772aa65190844e
78346593+chubatovatiger <78346593+chubatovatiger@TeamCity>
2022-03-07 08:45:43 +0100

    TeamCity change in 'SplitTests / Gradle' project: VCS roots of 'dep1nosplit' build configuration were updated

--- .teamcity/patches/buildTypes/Dep1nosplit.kts
+++ .teamcity/patches/buildTypes/Dep1nosplit.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("Dep1nosplit")
     name = "dep1nosplit"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
