c5981c78a0f54d6429512b298d2c24fe5cc95956
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-21 15:41:34 +0100

    Update TestSuite1.java

--- src/test/java/my/TestSuite1.java
+++ src/test/java/my/TestSuite1.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({mytest1.class,mytest2.class})
+//@Suite.SuiteClasses({mytest1.class,mytest2.class})
+@Suite.SuiteClasses({mytest1.class})
 public class TestSuite1 {
 
 }
