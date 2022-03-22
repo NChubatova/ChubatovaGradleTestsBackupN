39408ec24dcae449d3e13e213082d3e95ff57a74
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-22 21:13:08 +0100

    Update and rename TestSuite1.java to TestSuite11.java

--- src/test/java/my/TestSuite11.java
+++ src/test/java/my/TestSuite11.java
@@ -7,6 +7,6 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 //@Suite.SuiteClasses({mytest1.class,mytest2.class})
 @Suite.SuiteClasses({mytest1.class})
-public class TestSuite1 {
+public class TestSuite11 {
 
 }
