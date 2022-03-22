466b8558624464732cecfbf17e238e12be945570
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-22 20:44:20 +0100

    Create TestSuite2.java

---
+++ src/test/java/my/TestSuite2.java
@@ -0,0 +1,12 @@
+package my;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+//@Suite.SuiteClasses({mytest1.class,mytest2.class})
+@Suite.SuiteClasses({mytest2.class})
+public class TestSuite2 {
+
+}
