3dfb4ceb0cba20c1da29932b46cd8b597b983f17
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-04-06 14:17:15 +0200

    Create neck.java

---
+++ src/test/java/my/neck.java
@@ -0,0 +1,68 @@
+package my;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
+
+public class neck {
+
+    @Test
+    public void test81c() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+                 try 
+            {
+                //Thread.sleep(90000);
+            Thread.sleep(1500);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
+            //Assert.fail("fail mk");
+System.out.print("GfG1");
+        //};
+
+    };
+    @Test
+    public void test82c() {
+        try 
+            {
+                //Thread.sleep(90000);
+            Thread.sleep(1500);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+          //
+        };
+
+    };
+    @Test
+    public void test83c() {
+                 try 
+            {
+                //Thread.sleep(90000);
+            Thread.sleep(1500);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
+        if (System.currentTimeMillis()%1==1){
+        }
+        else {
+            
+            //Assert.fail("fail messajge2");
+            System.out.print("GfG1");
+        };
+
+    };
+
+}
