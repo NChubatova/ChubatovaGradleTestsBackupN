4d8113e5bb0f20a148b3dbb2e18b10fc84494c6a
nastasia.chubatova <nastasia.chubatova@jetbrains.com>
2022-05-31 04:37:40 -0700

    25kFiles

---
+++ src/test/java/my/UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest1554.java
@@ -0,0 +1,47 @@
+package my;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
+
+public class UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest1554 {
+
+    @Test
+    public void foo1master() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+            Assert.fail("fail mkk");
+
+        //};
+
+    };
+    @Test
+    public void foo2() {
+        try 
+            {
+                Thread.sleep(900);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+          //
+        };
+
+    };
+    @Test
+    public void foo3() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+       // else {
+            Assert.fail("fail messajge2");
+        //};
+
+    };
+
+}
