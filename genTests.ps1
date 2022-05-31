a0553428b6707e09abd4e2e475f5215e299b04ab
nastasia.chubatova <nastasia.chubatova@jetbrains.com>
2022-05-31 04:25:00 -0700

    Add3longtests

--- genTests.ps1
+++ genTests.ps1
@@ -1,5 +1,5 @@
-for (($i = 0); $i -lt 25; $i++)
+for (($i = 0); $i -lt 3; $i++)
 {
  $m='UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest' + $i;
- (Get-Content .\src\test\java\my\mytest1.java) -Replace 'mytest1', $m | Set-Content .\src\test\java\my\mytest$i.java
+ (Get-Content .\src\test\java\my\mytest.java) -Replace 'mytest1', $m | Set-Content .\src\test\java\my\UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest$i.java
 }
