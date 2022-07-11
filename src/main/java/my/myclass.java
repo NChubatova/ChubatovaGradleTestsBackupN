8e1df2596d83d9645ff4d56cf4312fb4f6c6c3ca
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-07-11 16:53:18 +0200

    Update myclass.java

--- src/main/java/my/myclass.java
+++ src/main/java/my/myclass.java
@@ -5,8 +5,13 @@ public class myclass {
     public static void main(String[] args) {
         int data=50;
         System.out.println("Hi from myclass45");
-        for (int i = 0; i < 4000000; i++) {
-        System.out.println(i + "..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from " + i);
+        for (int i = 0; i < 20000; i++) {
+            for (int j = 0; j < 20000; j++) {
+            System.out.println(##teamcity[blockOpened name=i description=i]);
+        System.out.println(j + "..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from " + j);
 }
     }
+    }
 }
+
+
