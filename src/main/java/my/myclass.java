6703383d31b315aa4ed1d0760a58258ba7a40905
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-07-11 15:41:59 +0200

    Update myclass.java

--- src/main/java/my/myclass.java
+++ src/main/java/my/myclass.java
@@ -5,5 +5,8 @@ public class myclass {
     public static void main(String[] args) {
         int data=50;
         System.out.println("Hi from myclass45");
+        for (int i = 0; i < 50; i++) {
+        System.out.println(i + "..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ..hi from ) + i);
+}
     }
 }
