73991beb4848c613ed67c24d388d9355ba28773d
AChubatova <n.chubatova@gmail.com>
2022-07-11 18:46:59 +0200

    a

--- genTests.ps1
+++ genTests.ps1
@@ -1,4 +1,4 @@
-for (($i = 0); $i -lt 25; $i++)
+for (($i = 0); $i -lt 25000; $i++)
 {
  $m='UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest' + $i;
  (Get-Content .\src\test\java\my\mytest.java) -Replace 'mytest', $m -Replace 'fail', 'notfail' | Set-Content .\src\test\java\my\UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest$i.java
