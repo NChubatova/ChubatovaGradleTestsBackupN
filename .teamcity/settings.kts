7f9a76d54195ae220e5232cbfd6adb496fca00a8
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-07-21 14:57:21 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.projectFeatures.activeStorage
-import jetbrains.buildServer.configs.kotlin.projectFeatures.s3Storage
 
 
 
@@ -11,22 +9,7 @@ project {
 
     buildType(Arts)
 
-    features {
-        s3Storage {
-            id = "PROJECT_EXT_4"
-            bucketName = "chubatovatransferacceleration"
-            accessKey = "credentialsJSON:1452225c-83d9-4a40-9171-f7f9e4ab9a5c"
-            awsEnvironment = custom {
-                endpoint = "https://chubatovatransferacceleration.s3-accelerate.amazonaws.com/"
-                awsRegionName = "eu-central-1"
-            }
-            accessKeyID = "mykey"
-        }
-        activeStorage {
-            id = "PROJECT_EXT_5"
-            activeStorageID = "PROJECT_EXT_4"
-        }
-    }
+
 }
 
 object Arts : BuildType({
