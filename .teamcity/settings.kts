de0ce080d57aff3b4ff4c753397ab96f0864d1c0
admin <admin@TeamCity>
2022-07-21 15:05:51 +0200

    TeamCity change in 's32' project: Artifacts storage settings updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -34,13 +34,14 @@ project {
     features {
         s3Storage {
             id = "PROJECT_EXT_6"
-            bucketName = "n.chubatova-test"
+            bucketName = "chubatovatransferacceleration"
             forceVirtualHostAddressing = true
             awsEnvironment = default {
                 awsRegionName = "eu-central-1"
             }
             accessKeyID = "AKIA5JH2VERVHVMPJQJI"
             accessKey = "credentialsJSON:4f0dd7f7-9e59-443b-8487-fc73ea89b097"
+            param("storage.s3.accelerateModeEnabled", "true")
         }
         activeStorage {
             id = "PROJECT_EXT_7"
