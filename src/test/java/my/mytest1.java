310414e9003d7802d43dca05b8cf013a55d045cd
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-08-05 14:03:31 +0200

    Update mytest1.java

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -12,7 +12,7 @@ public class mytest1 {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
-            Assert.fail("fail mkk_remtdde0");
+            //Assert.fail("fail mkk_remtdde0");
 
         //};
 
@@ -23,7 +23,7 @@ public class mytest1 {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
-            Assert.fail("fail mkk_remote2");
+            //Assert.fail("fail mkk_remote2");
 
         //};
 
@@ -33,7 +33,7 @@ public class mytest1 {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
-            Assert.fail("fail mkk_remote2");
+            //Assert.fail("fail mkk_remote2");
 
         //};
 
