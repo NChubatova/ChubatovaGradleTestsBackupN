019cd6c5ec62e5c4900d40225e3b21a992e4878e
AChubatova <n.chubatova@gmail.com>
2022-09-07 22:24:19 +0200

    remote_idea

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -8,7 +8,7 @@ import java.lang.*;
 public class mytest1 {
 
     @Test
-    public void foo2masterIDEapatch4_admin405() {
+    public void foo2masterIDEapatch4_admin407() {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
