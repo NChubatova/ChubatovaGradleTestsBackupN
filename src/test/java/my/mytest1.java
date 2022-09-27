2b3d9aa45e06014d0ac9a69bb99ba05790404102
AChubatova <n.chubatova@gmail.com>
2022-09-27 16:35:21 +0200

    kjkjk

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -8,7 +8,7 @@ import java.lang.*;
 public class mytest1 {
 
     @Test
-    public void foo2masterIDEapatch4_admin_23() {
+    public void foo2masterIDEapatch4_admin_29() {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
