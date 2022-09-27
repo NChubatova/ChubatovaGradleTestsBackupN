1d7aa61af7904057993ab1183e4736a1a28c9090
admin <admin@TeamCity>
2022-09-27 17:54:47 +0200

    TeamCity change in 'abc' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/settings.kts
@@ -0,0 +1,110 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2022.04"
+
+project {
+
+    vcsRoot(HttpsGithubComChubatovaTigerDotNetTestsSamples)
+
+    buildType(Cba_4)
+
+    params {
+        param("projpar", "master2")
+    }
+}
+
+object Cba_4 : BuildType({
+    id("Cba")
+    name = "cba"
+
+    artifactRules = "parsec*=>."
+    buildNumberPattern = "%build.counter%-master"
+
+    params {
+        param("aa", "master")
+        param("env.env", "envmaster")
+        password("parsec", "credentialsJSON:a42e2243-4440-4b74-a541-65893bb2c686")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+        root(HttpsGithubComChubatovaTigerDotNetTestsSamples)
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            gradleWrapperPath = ""
+        }
+        script {
+            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+            scriptContent = "echo master"
+        }
+        script {
+            name = "New build step"
+            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+            scriptContent = "echo %parsec% > parsec.txt"
+        }
+        script {
+            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+            scriptContent = "exit 1"
+        }
+    }
+
+    failureConditions {
+        testFailure = false
+        nonZeroExitCode = false
+    }
+
+    features {
+        parallelTests {
+            numberOfBatches = 3
+        }
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:25aa9774-b33d-4ec7-9045-8ec357f54265"
+                }
+            }
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.name", "Default")
+    }
+})
+
+object HttpsGithubComChubatovaTigerDotNetTestsSamples : GitVcsRoot({
+    name = "https://github.com/ChubatovaTiger/DotNetTestsSamples"
+    url = "https://github.com/ChubatovaTiger/DotNetTestsSamples"
+    branch = "refs/heads/master"
+})
