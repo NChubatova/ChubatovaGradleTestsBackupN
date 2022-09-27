8bad6d1321084b75c097d73353e239a1b218f76e
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-09-27 18:09:29 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,14 +1,4 @@
-To debug settings scripts in command-line, run the
 
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2022.04"
 
