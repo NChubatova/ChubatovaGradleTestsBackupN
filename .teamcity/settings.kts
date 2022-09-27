9a91f39db4025356d6593eb197ac26801ea4abdf
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-09-27 17:58:38 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,27 +5,6 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2022.04"
 
@@ -36,7 +15,7 @@ project {
     buildType(Cba_4)
 
     params {
-        param("projpar", "b4-ranch")
+        param("projpar", "master")
     }
 }
 
@@ -45,12 +24,12 @@ object Cba_4 : BuildType({
     name = "cba"
 
     artifactRules = "parsec*=>."
-    buildNumberPattern = "%build.counter%-b4-ranch"
+    buildNumberPattern = "%build.counter%-master"
 
     params {
-        param("aa", "b4-ranch")
-        param("env.env", "envb4ranch")
-        password("parsec", "credentialsJSON:6f0b7139-8ec4-401c-bd16-d847f78387a9")
+        param("aa", "master")
+        param("env.env", "envmaster")
+        password("parsec", "credentialsJSON:25aa9774-b33d-4ec7-9045-8ec357f54265")
     }
 
     vcs {
@@ -65,12 +44,12 @@ object Cba_4 : BuildType({
         }
         script {
             executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
-            scriptContent = "echo b4ranch"
+            scriptContent = "echo master"
         }
         script {
             name = "New build step"
             executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
-            scriptContent = "echo %parsec% > parsecb4ranch.txt"
+            scriptContent = "echo %parsec% > parsecmaster.txt"
         }
         script {
             executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
@@ -99,7 +78,7 @@ object Cba_4 : BuildType({
     }
 
     requirements {
-        contains("teamcity.agent.name", "nc")
+        contains("teamcity.agent.name", "Default")
     }
 })
 
