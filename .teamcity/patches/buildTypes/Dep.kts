0fa4d695930c7009b95f75050d4c05ebfb67a15c
admin <admin@TeamCity>
2022-09-27 19:30:48 +0200

    TeamCity change in 'abc' project: VCS roots of 'dep' build configuration were updated

--- .teamcity/patches/buildTypes/Dep.kts
+++ .teamcity/patches/buildTypes/Dep.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("Dep")
     name = "dep"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
