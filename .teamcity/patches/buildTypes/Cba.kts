c597c15d4884bd9c072f71b117552a3e2d842bcb
admin <admin@TeamCity>
2022-09-28 12:51:44 +0200

    TeamCity change in 'abc' project: 'cba' build configuration settings were updated

--- .teamcity/patches/buildTypes/Cba.kts
+++ .teamcity/patches/buildTypes/Cba.kts
@@ -1,8 +1,6 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.ParallelTestsFeature
-import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -11,17 +9,6 @@ To apply the patch, change the buildType with id = 'Cba'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Cba")) {
-    features {
-        val feature1 = find<ParallelTestsFeature> {
-            parallelTests {
-                numberOfBatches = 3
-            }
-        }
-        feature1.apply {
-            enabled = false
-        }
-    }
-
     dependencies {
         remove(RelativeId("Dep")) {
             snapshot {
