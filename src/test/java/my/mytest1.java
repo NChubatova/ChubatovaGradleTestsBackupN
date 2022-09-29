f7af30bc6974258294caf68f29dc0e9bcd7e2d2d
AChubatova <n.chubatova@gmail.com>
2022-09-29 14:19:09 +0200

    ange2y

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -8,7 +8,7 @@ import java.lang.*;
 public class mytest1 {
 
     @Test
-    public void test1_change11() {
+    public void test1_change21() {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
