bbdf253274cd4979cd36bcb11c6fb8f9625854ec
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-09-29 10:43:20 +0200

    Update mytest1.java

--- src/test/java/my/mytest1.java
+++ src/test/java/my/mytest1.java
@@ -8,7 +8,7 @@ import java.lang.*;
 public class mytest1 {
 
     @Test
-    public void foo2masterIDEapatch4_admin_29() {
+    public void test1_change1() {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
