4012dabad5cb8dccd30fdd6e73508a16ee7b9299
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-10-11 13:20:11 +0200

    Create mytest4.java

---
+++ src/test/java/my/mytest4.java
@@ -0,0 +1,43 @@
+package my;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
+//haha
+public class mytest4 {
+
+    @Test
+    public void test41() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+            Assert.fail("fail mkk_r");
+
+        //};
+
+    };
+    
+    @Test
+    public void test42() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+            //Assert.fail("fail mkk_remote2");
+
+        //};
+
+    };
+        @Test
+    public void test43() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+            //Assert.fail("fail mkk_remote2");
+
+        //};
+
+    };
+
+
+}
