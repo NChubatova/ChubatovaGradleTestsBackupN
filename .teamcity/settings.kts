2b724f00599f8020772aaea5541f3cc7125787ed
admin <admin@TeamCity>
2022-11-04 11:21:38 +0000

    TeamCity change in 'aaa' project: 'maven' build configuration was created

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -25,4 +25,10 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2022.10"
 
 project {
+
+    buildType(Maven)
 }
+
+object Maven : BuildType({
+    name = "maven"
+})
