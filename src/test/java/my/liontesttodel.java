a73b5547ad3010a2da90f052413ca5fb2aaffba9
nodoby nobodin <n.chubatova@gmail.com>
2022-12-20 14:04:58 +0100

    add todel

---
+++ src/test/java/my/liontesttodel.java
@@ -0,0 +1,40 @@
+package my;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//haha
+public class liontesttodel {
+
+    @Test
+    public void test41flk1() {
+        if (System.currentTimeMillis()%2==1){
+        }
+        else {
+            Assert.fail("f.  js");
+
+        };
+
+    };
+    
+    @Test
+    public void test42s() {
+        //if (System.currentTimeMillis()%3==1){
+        //}
+        //else {
+            //Assert.fail("fail mkk_remote2");
+
+        //};
+
+    };
+        @Test
+    public void test43s() {
+
+            Assert.fail("fail mkk_remote2");
+
+
+
+    };
+
+
+}
