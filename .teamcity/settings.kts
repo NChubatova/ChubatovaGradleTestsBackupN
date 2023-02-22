bfd0ced39bbb67704b91ab7d2a0fd5c82ac12d00
admin <admin@TeamCity>
2023-02-22 12:13:48 +0100

    TeamCity change in 'project1' project: 'b1pr1' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -87,6 +87,7 @@ object Project1_B1pr1 : BuildType({
 
     steps {
         script {
+            enabled = false
             scriptContent = """
                 mkdir dir1
                 echo a > dir1/1-%build.counter%
