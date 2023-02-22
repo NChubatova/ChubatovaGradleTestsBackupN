04dbce9bde64ecb6b292bd0d2afa688075142e4c
admin <admin@TeamCity>
2023-02-22 12:21:29 +0100

    TeamCity change in 'project1' project: build features of 'b1pr1' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -101,7 +101,7 @@ object Project1_B1pr1 : BuildType({
     features {
         buildCache {
             name = "mycache2"
-            rules = "myDir"
+            rules = "%dir%"
         }
     }
 })
