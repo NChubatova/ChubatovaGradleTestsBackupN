050ed2913553c0551ac7aee9b7b5668c831ab4cd
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-04-12 20:58:28 +0200

    Update mytest2.java

--- src/test/java/my/mytest2.java
+++ src/test/java/my/mytest2.java
@@ -23,7 +23,7 @@ public class mytest2 {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
-            Assert.fail("fail mkk_remote2");
+            //Assert.fail("fail mkk_remote2");
 
         //};
 
@@ -33,7 +33,7 @@ public class mytest2 {
         //if (System.currentTimeMillis()%3==1){
         //}
         //else {
-            Assert.fail("fail mkk_remote2");
+            //Assert.fail("fail mkk_remote2");
 
         //};
 
