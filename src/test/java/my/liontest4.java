985890c843fd34a409f21c3e1373ca7281440de4
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-04-12 21:01:55 +0200

    Update liontest4.java

--- src/test/java/my/liontest4.java
+++ src/test/java/my/liontest4.java
@@ -12,7 +12,7 @@ public class liontest4 {
         if (System.currentTimeMillis()%2==1){
         }
         else {
-            Assert.fail("f.  j");
+            //Assert.fail("f.  j");
 
         };
 
@@ -31,7 +31,7 @@ public class liontest4 {
         @Test
     public void test43s() {
 
-            Assert.fail("fail mkk_remote2");
+           // Assert.fail("fail mkk_remote2");
 
 
 
