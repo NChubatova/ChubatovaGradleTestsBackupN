33f34bf5308e7a4c3e7d632b96600f81090dc3fd
admin <admin@TeamCity>
2023-11-16 18:06:03 +0100

    TeamCity change in 'project2' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/_Self/Project.kt
@@ -0,0 +1,39 @@
+package _Self
+
+import _Self.buildTypes.*
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.amazonEC2CloudImage
+import jetbrains.buildServer.configs.kotlin.amazonEC2CloudProfile
+
+object Project : Project({
+
+    buildType(Build3)
+    buildType(Build2)
+    buildType(Build1)
+    buildType(Build4)
+
+    template(Tmpl)
+
+    features {
+        amazonEC2CloudImage {
+            id = "PROJECT_EXT_2"
+            profileId = "amazon-1"
+            agentPoolId = "-2"
+            vpcSubnetId = "subnet-043178c302cabfe37"
+            keyPairName = "chubatova-amazon-ireland"
+            instanceType = "t3.medium"
+            securityGroups = listOf("sg-072d8bfa0626ea2a6")
+            source = Source("ami-0b55e6eb21b887f36")
+        }
+        amazonEC2CloudProfile {
+            id = "amazon-1"
+            name = "a"
+            terminateIdleMinutes = 0
+            region = AmazonEC2CloudProfile.Regions.EU_WEST_DUBLIN
+            authType = instanceIAMRole()
+            param("secure:access-id", "credentialsJSON:f67de2dd-6349-4a0a-8a7a-b99d61bf6447")
+            param("secure:secret-key", "credentialsJSON:f67de2dd-6349-4a0a-8a7a-b99d61bf6447")
+        }
+    }
+})
---
+++ .teamcity/_Self/buildTypes/Build4.kt
@@ -0,0 +1,17 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.matrix
+
+object Build4 : BuildType({
+    name = "build4"
+
+    features {
+        matrix {
+            param("custim12", listOf(
+                value("13"),
+                value("14")
+            ))
+        }
+    }
+})
