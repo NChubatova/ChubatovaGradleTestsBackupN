253adf298c7bdda7fdafb3b9fe06d8c3f0ff73cd
admin <admin@TeamCity>
2023-11-16 19:01:13 +0100

    TeamCity change in 'project2' project: build features of 'build4' build configuration were updated

--- .teamcity/_Self/buildTypes/Build4.kt
+++ .teamcity/_Self/buildTypes/Build4.kt
@@ -1,6 +1,7 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.approval
 import jetbrains.buildServer.configs.kotlin.matrix
 
 object Build4 : BuildType({
@@ -12,6 +13,9 @@ object Build4 : BuildType({
                 value("3"),
                 value("343")
             ))
+        }
+        approval {
+            approvalRules = "user:admin"
         }
     }
 })
