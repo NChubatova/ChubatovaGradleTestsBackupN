e674b9de4ad094f59eaeecb5249c1412fff48656
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-11-16 15:47:41 +0100

    Update Build2.kt

--- .teamcity/_Self/buildTypes/Build2.kt
+++ .teamcity/_Self/buildTypes/Build2.kt
@@ -11,8 +11,25 @@ object Build2 : BuildType({
     }
 
     features {
-        approval {
-            approvalRules = "user:admin"
+         matrix {
+            param("OS", listOf(
+                value("Linux"),
+                value("Windows"),
+                value("Windows2")
+            ))
+            param("env.JAVA_HOME", listOf(
+                value("%env.JDK_21_0%", label = "JDK 21"),
+                value("%env.JDK_11%", label = "JDK 11")
+            ))
+            param("custom2", listOf(
+                value("a"),
+                value("b")
+            ))
+            param("customwitjlabel", listOf(
+                value("a"),
+                value("B", label = "my labelB")
+            ))
         }
+
     }
 })
