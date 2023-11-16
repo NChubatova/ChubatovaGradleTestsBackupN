0d56f11aac2af237c5258a75530e2402c7d6287d
admin <admin@TeamCity>
2023-11-16 15:58:19 +0100

    Matrix Build feature was created via user interface

--- .teamcity/_Self/buildTypes/Build3.kt
+++ .teamcity/_Self/buildTypes/Build3.kt
@@ -1,7 +1,17 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.matrix
 
 object Build3 : BuildType({
     name = "build3"
+
+    features {
+        matrix {
+            param("par1", listOf(
+                value("1"),
+                value("2")
+            ))
+        }
+    }
 })
