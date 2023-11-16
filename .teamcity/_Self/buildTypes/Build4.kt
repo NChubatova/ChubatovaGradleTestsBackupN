1a07b5cf4a05be5ed95e6f8aec4ebea8b4b6cfaf
admin <admin@TeamCity>
2023-11-16 18:10:08 +0100

    TeamCity change in 'project2' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/_Self/buildTypes/Build4.kt
@@ -0,0 +1,17 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.matrix
+
+object Build4 : BuildType({
+    name = "build4"
+
+    features {
+        matrix {
+            param("par2", listOf(
+                value("3"),
+                value("343")
+            ))
+        }
+    }
+})
