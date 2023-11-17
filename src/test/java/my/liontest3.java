aa81e3fe7758cadc7fa2f075421ad9da21733c5b
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-11-17 17:03:47 +0100

    Update liontest3.java

--- src/test/java/my/liontest3.java
+++ src/test/java/my/liontest3.java
@@ -12,7 +12,7 @@ public class liontest3 {
         //if (System.currentTimeMillis()%2==1){
         //}
         //else {
-            ..Assert.fail("fad");
+            //Assert.fail("fad");
 
         //};
 
