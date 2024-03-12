b273f106e78b37cfcbbb9cdcaef9ac3e00282d8d
admin <admin@TeamCity>
2024-03-12 21:40:25 +0100

    TeamCity change in 'prRs' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/settings.kts
@@ -0,0 +1,92 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2023.11"
+
+project {
+
+    buildType(Test2)
+    buildType(Test1)
+    buildType(BuildAll)
+}
+
+object BuildAll : BuildType({
+    name = "BuildAll"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    dependencies {
+        snapshot(Test1) {
+        }
+        snapshot(Test2) {
+        }
+    }
+})
+
+object Test1 : BuildType({
+    name = "Test1"
+
+    vcs {
+        root(DslContext.settingsRoot, "+:Project")
+    }
+
+    steps {
+        gradle {
+            id = "gradle_runner"
+            tasks = "clean build"
+            buildFile = "Project/build.gradle"
+            gradleWrapperPath = "Project"
+            jdkHome = "%env.JDK_11%"
+        }
+    }
+})
+
+object Test2 : BuildType({
+    name = "Test2"
+
+    vcs {
+        root(DslContext.settingsRoot, "+:Project")
+    }
+
+    steps {
+        gradle {
+            name = "New build step"
+            id = "gradle_runner"
+            tasks = "clean build"
+            buildFile = "Project/build.gradle"
+            gradleWrapperPath = "Project"
+            jdkHome = "%env.JDK_11%"
+        }
+    }
+
+    features {
+        parallelTests {
+            numberOfBatches = 2
+        }
+    }
+})
